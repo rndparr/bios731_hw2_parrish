1e94c0e55dd5d993c37946bc45672b2b89bc5015
Randy_Parrish <mparri4@node54.cl.emory.edu>
2026-02-08 14:54:45 -0500

    cleaned up run_sim_i.R, added comments

--- source/run_sim_i.R
+++ source/run_sim_i.R
@@ -1,6 +1,9 @@
 
-# Rscript run_sim_i.R ${i} ${ncores}
 
+###############################################################
+## PARSE ARGUMENTS
+###############################################################
+# Rscript run_sim_i.R ${i} ${ncores}
 
 # parse arguments
 args=(commandArgs(TRUE))
@@ -8,23 +11,22 @@ cat(paste0('\nargs:\n'))
 print(args)
 cat()
 if(length(args)==0) {
-	stop("Error: No arguments supplied!")
+	stop('Error: No arguments supplied!')
 } else  {
-	i = as.integer(args[[1]])
-	ncores = as.integer(args[[2]])
+	i <- as.integer(args[[1]])
+	ncores <- as.integer(args[[2]])
 }
 
 
 ###############################################################
 ## LOAD LIBRARIES, SET UP PARALLELIZATION
 ###############################################################
-
-# set up parallelization
+# libraries
 library(doFuture)
 library(doRNG)
 library(foreach)
 
-
+# set up parallel environment
 registerDoFuture()
 if (ncores > 1){
 	pln <- plan(multicore, workers=ncores)
@@ -35,7 +37,7 @@ if (ncores > 1){
 
 
 ###############################################################
-## define or source functions used in code below
+## SOURCE FUNCTIONS
 ###############################################################
 source(here::here('source', 'utility.R'))
 source(here::here('source', 'simulate_data.R'))
@@ -50,15 +52,16 @@ source(here::here('source', 'error_funcs.R'))
 
 alpha <- 0.05
 
+# job parameters
 n_sim <- 475
 B <- 500
 B_inner <- 100
 
 ## testing:
 # n_sim <- 20; B <- 10; B_inner <- 5;
-
 # i=1; j=1
 
+# scenario parameters
 n_sample_vals <- c(10, 50, 500)
 true_beta_tx_vals <- c(0, 0.5, 2)
 error_dists <- setNames(
@@ -75,36 +78,50 @@ params <- expand.grid(
 
 
 ###############################################################
-# RUN SIMULATIONS
+# LOGGING
 ###############################################################
 
+# print job parameter info to file
+print('Job parameters:')
+print(params[i,])
+print(paste0('B=', B, '; B_inner=', B_inner, '; alpha=', alpha))
+print(paste0('scenario ', i))
+
+# print session info to file
+sI <- sessionInfo()
+print(sI)
+
+# create log for keeping track of j sims for running jobs
+out_log <- here::here('logs', paste0('scenario_', i, 'tasks.txt'))
+
+
+###############################################################
+# RUN SIMULATIONS
+###############################################################
 
 # initial seed to generate other seeds
 set.seed(02072026)
 seed <- sample(1:10000, n_sim, replace=FALSE)
 
 
-# loop over scenarios
-# for (i in 1:nrow(params)) {
-print('Job parameters:')
-print(params[i,])
-print(paste0('B=', B, '; B_inner=', B_inner, '; alpha=', alpha))
-print(paste0('scenario ', i))
-
-out_log <- here::here('logs', paste0('scenario_', i, 'tasks.txt'))
 # foreach combine n_sims for scenario i
 scenario_i_output <- foreach(j=1:n_sim, .combine=rbind) %dorng% {
-	# message every time start 25th sim
-	# if( (j %% 5) == 0 ) { cat(paste0('j=', j, '\n'), file=out_log, append=TRUE) }
+
+	# output j to file
 	cat(paste0(j, '\n'), file=out_log, append=TRUE)
+
+	# set seed
 	set.seed(seed[j])
 
+	# generate simulation data
 	sim_data <- do.call(get_sim_data, params[i,])
 
+	# get wald estimates, time
 	wald_time <- func_time(
 		estimates <- get_wald_estimates(sim_data, true_beta=params[i, 'beta_tx'], alpha=alpha)
 	)
 
+	# get bootstrap estimates, time
 	boot_time <- func_time(
 		boot_estimates <- get_bootstrap_estimates(sim_data, 
 			beta_hat=estimates[['beta_hat']], 
@@ -112,49 +129,16 @@ scenario_i_output <- foreach(j=1:n_sim, .combine=rbind) %dorng% {
 			B=B, B_inner=B_inner, alpha=alpha)
 	)
 
+	# get bootp time by subtracting boott_time from total boot_time
 	bootp_time <- boot_time - boot_estimates[['boott_time']]
 
+	# results for simulation j
 	res_j <- cbind('scenario'=i, params[i,], B, B_inner, alpha, estimates, boot_estimates, bootp_time, wald_time, boot_time, 'seed'=seed[j])
 } 
 
-# save data
+# save all data for scenario i
 scenario_i_path <- here::here('data', paste0('scenario_', i, '.Rds'))
 save(scenario_i_output, file=scenario_i_path)
 
 print('All done!')
-
-# }
-
-
-
-# # merge data for all scenarios
-# big_data <- data.frame()
-# for (i in 1:nrow(params)) {
-# 	scenario_i_path <- here::here('data', paste0('scenario_', i, '.Rds'))
-# 	load(scenario_i_path)
-
-# 	big_data <- rbind(big_data, scenario_i_output)
-
-# }
-
-# bleh <- foreach(j=1:n_sim, .combine=rbind) %dorng% {
-# 	set.seed(seed[j])
-
-# 	sim_data <- do.call(get_sim_data, params[i,])
-
-# 	wald_time <- func_time(
-# 		estimates <- get_wald_estimates(sim_data, true_beta=params[i, 'beta_tx'], alpha=alpha)
-# 	)
-
-# 	boot_time <- func_time(
-# 		boot_estimates <- get_bootstrap_estimates(sim_data, 
-# 			beta_hat=estimates[['beta_hat']], 
-# 			true_beta=params[i, 'beta_tx'],
-# 			B=B, B_inner=B_inner, alpha=alpha)
-# 	)
-
-# 	res_j <- cbind(params[i,], estimates, boot_estimates, wald_time, boot_time, 'seed'=seed[j])
-# } 
-
-
 
